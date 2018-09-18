700dfb149e71a2282b7ba9a14f3e0065171cc422
KazuhiroObama <obamakazuhiro@gmail.com>
2018-09-19 06:49:16 +0900

    'users#show'に関するrouting表記変更 #3
    'users#show'をroutingに追加する方法を、「resources」メソッドから「devise_scope」メソッ
    ドに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root 'events#index'
+
   devise_for :users
+  #devise_forでは'user#show'ページが追加されないため、以下に設定
+  devise_scope :user do
+   get :show, to: 'users#show', as: 'users_show'
+  end
+
   resources :events do
     get :post, on: :collection
     resources :comments, only: [:create, :destroy]
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  resources :users, only: [:show]
   resources :managements, only: [:create, :destroy]
 end

4116156f6b13f6e201d09ad67fcfc945864927ec
KazuhiroObama <obamakazuhiro@gmail.com>
2018-09-18 13:24:16 +0900

    Clean #1 and clean #2
    トップページへのrouting記述方法を変更し、先頭の行に記述した

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'events#index'
   devise_for :users
   resources :events do
     get :post, on: :collection
     resources :comments, only: [:create, :destroy]
   end
-  root to: "events#index"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :users, only: [:show]
   resources :managements, only: [:create, :destroy]
